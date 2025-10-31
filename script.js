da729eefddd7edccfc4c4be2ead868af20b0eaf9
Ana Farmus <afarmus@purdue.edu>
2025-10-31 17:38:58 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+new FinisherHeader({
+  "count": 9,
+  "size": {
+    "min": 1300,
+    "max": 1500,
+    "pulse": 0
+  },
+  "speed": {
+    "x": {
+      "min": 0.6,
+      "max": 2.5
+    },
+    "y": {
+      "min": 0.6,
+      "max": 2.5
+    }
+  },
+  "colors": {
+    "background": "#c5ad90",
+    "particles": [
+      "#ff9e1c",
+      "#6fe3dd",
+      "#836dcf",
+      "#ea62b1"
+    ]
+  },
+  "blending": "lighten",
+  "opacity": {
+    "center": 0.6,
+    "edge": 0
+  },
+  "skew": 0,
+  "shapes": [
+    "c"
+  ]
+});
